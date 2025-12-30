2990ee100a0038cd496daa037a5b9c29749d105c
Лена Швечкова <alisamejr@gmail.com>
2025-12-30 17:29:49 +0400

    Test new negative

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,19 @@
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    TimeoutException,
+    NoAlertPresentException,
+)
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import math
+import time
 
 
 class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
+        # self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
@@ -31,3 +38,21 @@ class BasePage:
             alert.accept()
         except:
             print("No second alert presented")
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(
+                EC.presence_of_element_located((how, what))
+            )
+        except TimeoutException:
+            return False
+        return True
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(
+                EC.presence_of_element_located((how, what))
+            )
+        except TimeoutException:
+            return False
+        return True
--- pages/product_page.py
+++ pages/product_page.py
@@ -60,4 +60,14 @@ class ProductPage(BasePage):
 
         assert (
             product_price == basket_total
-        ), f"Basket total doesn't match product price. Experted: '{product_price}', got: '{basket_total}'"
+        ), f"Basket total doesn't match product price. Expected: '{product_price}', got: '{basket_total}'"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(
+            *ProductPageLocators.SUCCESS_MESSAGE
+        ), "Success message is presented, but should not be"
+
+    def should_be_success_message_disappeared(self):
+        assert self.is_disappeared(
+            *ProductPageLocators.SUCCESS_MESSAGE
+        ), "Success message is not disappesred, but should bel"
